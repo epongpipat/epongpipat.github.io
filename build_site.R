8f8f804d3c6a5753f88322f9104835557ecda8f7
Ekarin Eric Pongpipat <epongpipat@gmail.com>
2019-02-20 12:04:49 -0600

    blog-update

---
+++ build_site.R
@@ -0,0 +1,11 @@
+# load packages
+packages <- c("tidyverse")
+xfun::pkg_attach(packages, message = F)
+
+# read .Rmd files in directory
+# do not include files that are still being edited
+files <- list.files(pattern = ".Rmd") %>%
+  str_subset(string = ., pattern = "editing", negate = T)
+
+# render site
+lapply(files, rmarkdown::render_site)
